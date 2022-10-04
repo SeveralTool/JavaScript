971799d06406666ecc816c9aa4d5cdefc1af7b43
Nahuel Galeano <40505451+Nahuel-7@users.noreply.github.com>
2022-10-04 17:10:23 -0300

    Create changeEmail.js
    Modificar email de un usario con ajax en js

---
+++ changeEmail.js
@@ -0,0 +1,17 @@
+$(document).on('click', '#correo', function(e){
+    e.preventDefault()
+    let valor = $(this)[0].parentElement
+    correo = $(valor).find('input').val()
+    console.log(correo)
+    if(correo.length >= 7 && /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(correo) == true ) {
+        $.post('update.php',{correo}, function(response){
+            console.log(response)
+        })
+    }else{
+        $('#CORREO').val("Ingrese un correo valido")
+        $('#CORREO').css("border-color","red")
+        setTimeout(() => {
+            $('#CORREO').val("")    
+        }, 3000);
+    }
+})
